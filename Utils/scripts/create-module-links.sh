0416bae18392ce4c3f33ef96fd29e68c445a73b7
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-05-06 21:25:31 +0200

    Cleanup create-module-link.sh
    Activate unofficial bash strict mode
    Add logging facility
    Add log messages
    Don't fail if node_modules directory already exists
    Don't fail if symbolic links exist; warn instead
    Use subshell instead of cd into a folder and back
    Use basename to get folder name from path instead of relying on another
    array

--- Utils/scripts/create-module-links.sh
+++ Utils/scripts/create-module-links.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 ########################################################################################################
 # File      : ./scripts/create-module-links.sh                                                         #
-# Author(s) : Zidmann (emmanuel.zidel@gmail.com)                                                       #
+# Author(s) : Zidmann (emmanuel.zidel@gmail.com), Nymous                                               #
 # Function  : Script to create the links inside node_modules inside all NodeJS servers                 #
-# Version   : 1.0.0                                                                                    #
+# Version   : 1.1.0                                                                                    #
 ########################################################################################################
 
 
 SCRIPT_DIR=$(dirname "$0")
+# shellcheck source=sources/sources.sh
 source $SCRIPT_DIR"/sources/sources.sh"
-CURRENT_DIR=`pwd`
+# shellcheck source=sources/logger.sh
+source "$SCRIPT_DIR/sources/logger.sh"
 
+info "Linking modules"
+(
+  cd $DEV_DIR || exit
+  info "Creating node_modules directory"
+  mkdir -p node_modules/ 1>/dev/null 2>&1
 
-cd $DEV_DIR
-mkdir node_modules/ 2>&1 1>/dev/null
-
-for i in ${!dir_module_path[@]};
-do
-	ln -s ../${dir_module_path[i]} node_modules/${dir_module_name[i]} 2>&1 1>/dev/null
-done
-
-cd $CURRENT_DIR
+  for module_path in "${dir_module_path[@]}";
+  do
+    module_name=$(basename "$module_path")
+    if [[ -h node_modules/$module_name ]]
+      then warning "Symbolic link $DEV_DIR/node_modules/$module_name already exists"
+    else
+      info "Creating link called $DEV_DIR/node_modules/$module_name pointing to $DEV_DIR/../$module_path"
+      ln -s "../$module_path" "node_modules/$module_name" 1>/dev/null 2>&1
+    fi
+  done
+)
