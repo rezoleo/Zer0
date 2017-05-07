617dc6a393f11e5ec50c74bf06f9a534edc96b21
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2017-05-07 18:44:52 +0200

    Cleanup install-modules.sh
    Activate unofficial bash strict mode
    Add logging facility
    Add log messages
    Add counter to know how many projects there are
    Use locally installed bower instead of global one, if it is available
    Exit if bower isn't found
    Use subshells instead of cd into a folder and back
    Use values instead of index to access arrays

--- Utils/scripts/install-modules.sh
+++ Utils/scripts/install-modules.sh
@@ -1,47 +1,79 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 ########################################################################################################
 # File      : ./scripts/install-modules.sh                                                             #
-# Author(s) : Zidmann (emmanuel.zidel@gmail.com)                                                       #
+# Author(s) : Zidmann (emmanuel.zidel@gmail.com), Nymous                                               #
 # Function  : Script to install the different dependancies in node_modules/ and bower_components/      #
 #             directories                                                                              #
-# Version   : 1.0.0                                                                                    #
+# Version   : 1.1.0                                                                                    #
 ########################################################################################################
 
 
 SCRIPT_DIR=$(dirname "$0")
-source $SCRIPT_DIR"/sources/sources.sh"
-CURRENT_DIR=`pwd`
+# shellcheck source=sources/sources.sh
+source "$SCRIPT_DIR/sources/sources.sh"
+# shellcheck source=sources/logger.sh
+source "$SCRIPT_DIR/sources/logger.sh"
 
-echo "-------------------------------"
-for i in ${!dir_tab[@]};
+export bower_command
+
+if [[ -x $(cd "$SCRIPT_DIR/../.."; npm bin)/bower ]]
+  then bower_command=$(cd "$SCRIPT_DIR/../.."; npm bin)/bower
+elif hash bower 2>/dev/null
+  then bower_command=bower
+else
+  fatal "Bower was not found!"
+fi
+
+info "Installing npm and bower modules"
+info "-------------------------------"
+
+export counter
+export length
+
+info "Installing npm modules for servers"
+info "-------------------------------"
+counter=1
+length=${#dir_tab[@]}
+for server_path in "${dir_tab[@]}"
 do
-	cd $DEV_DIR
-	cd ${dir_tab[i]};
-	echo "Installation in the directory ${dir_tab[i]}"
-	npm i
-	echo "-------------------------------"
-	cd $CURRENT_DIR
+  (
+    cd "$DEV_DIR/$server_path" || exit
+    info "[$counter/$length] - Installation in the directory $DEV_DIR/$server_path"
+    npm i
+  )
+  ((counter++))
 done
 
-for j in ${!dir_module_path[@]};
+info "-------------------------------"
+info "Installing npm modules for modules"
+info "-------------------------------"
+counter=1
+length=${#dir_module_path[@]}
+for module_path in "${dir_module_path[@]}"
 do
-	cd $DEV_DIR
-	cd ${dir_module_path[j]};
-	echo "Installation in the directory ${dir_module_path[j]}"
-	npm i
-	echo "-------------------------------"
-	cd $CURRENT_DIR
+  (
+    cd "$DEV_DIR/$module_path" || exit
+    info "[$counter/$length] - Installation in the directory $DEV_DIR/$module_path"
+    npm i
+  )
+  ((counter++))
 done
 
-for k in ${!dir_front_path[@]};
+info "-------------------------------"
+info "Installing npm and bower modules for front-end apps"
+info "-------------------------------"
+counter=1
+length=${#dir_front_path[@]}
+for front_app_path in "${dir_front_path[@]}"
 do
-	cd $DEV_DIR
-	cd ${dir_front_path[k]};
-	echo "Installation in the directory ${dir_front_path[k]}"
-	npm i
-	bower i
-	echo "-------------------------------"
-	cd $CURRENT_DIR
+  (
+    cd "$DEV_DIR/$front_app_path" || exit
+    info "[$counter/$length] - Installation in the directory $DEV_DIR/$front_app_path"
+    npm i
+    $bower_command i
+  )
+  ((counter++))
 done
-
