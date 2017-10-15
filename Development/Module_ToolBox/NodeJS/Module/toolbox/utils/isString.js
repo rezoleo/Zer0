a9cb0bf13e523a629c4378a0ed990108a558312e
zidmann <Zidmann>
2017-10-15 11:28:54 +0200

    Fix one problem in ISSTRING function in the toolbox.
    Signed-off-by: zidmann <Zidmann>

--- Development/Module_ToolBox/NodeJS/Module/toolbox/utils/isString.js
+++ Development/Module_ToolBox/NodeJS/Module/toolbox/utils/isString.js
@@ -2,7 +2,7 @@
  * File 	: ./utils/isString.js
  * Author(s)	: Zidmann
  * Function 	: This file defines the 'isString' function to know if an element is a string
- * Version  	: 1.0.0
+ * Version  	: 1.0.1
  */
 
 var isEmpty = require('./isEmpty.js');
@@ -11,7 +11,7 @@ function isStringFunc(obj){
 	if(isEmpty(obj)){
 		return false;
 	}
-	return (typeof(obj)==='string' && obj instanceof String);
+	return (typeof(obj)==='string'/* && obj instanceof String*/);
 }
 
 
