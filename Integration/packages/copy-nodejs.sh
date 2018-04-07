3b1f561df3b5a3f69247e10007051bfca95c973a
Zidmann <emmanuel.zidel@gmail.com>
2018-04-07 10:52:39 +0200

    Update the Integration branch script to check if user is or isn't root user.
    Signed-off-by: Zidmann <emmanuel.zidel@gmail.com>

--- Integration/packages/copy-nodejs.sh
+++ Integration/packages/copy-nodejs.sh
@@ -4,10 +4,19 @@
 # File      : ./copy-nodejs.sh                                                                         #
 # Author(s) : Zidmann (emmanuel.zidel@gmail.com)                                                       #
 # Function  : Script to copy NodeJS sources from Development directory                                 #
-# Version   : 1.0.0                                                                                    #
+# Version   : 1.1.0                                                                                    #
 #             The script was tested on Ubuntu 14.04                                                    #
 ########################################################################################################
 
+
+# Root Id
+ROOT_UID=0
+
+#Check if user is not root
+if [ $UID == $ROOT_UID ]; then
+	echo -e "[-]Error : User must NOT be root to copy NodeJs files."
+	exit 0
+fi
 
 SCRIPT_DIR=$(dirname "$0")
 source $SCRIPT_DIR"/../../Utils/scripts/sources/sources.sh"
